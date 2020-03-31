fbbe9ce089bd84c809e7b210aeb3fae8ac6ee48d
FichteFoll <fichtefoll2@googlemail.com>
2020-03-31 21:27:30 +0200

    WIP: Split page in two (sidebars) and move things around

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,26 +17,26 @@ const config = {
             // defaultColorTheme: 'orange',
         },
         repo: 'sublimetext-io/docs.sublimetext.io',
-        docsBranch: 'docs',
+        docsDir: 'docs',
         editLinks: true,
         sidebarDepth: 3,
         lastUpdated: true,
         nav: [
             {
                 title: 'Guide',
-                link: '/views/glossary/',
+                link: '/guide/',
             },
             {
                 title: 'Reference',
-                path: '/views/reference/',
+                link: '/reference/',
             },
             {
                 title: 'Glossary',
-                link: '/views/glossary/',
+                link: '/glossary',
             },
             {
                 text: 'F.A.Q',
-                link: '/views/other/question',
+                link: '/other/question',
             },
             {
                 text: 'Offical Docs',
@@ -44,113 +44,126 @@ const config = {
             },
         ],
         sidebar: {
-            {
-                title: 'Introduction',
-                path: '/',
-                collapsable: false,
-                sidebarDepth: 3,
-                children: [
-                    '/views/',
-                    '/views/installation/',
-                    '/views/basic-concepts/'
-                ],
-            },
-            {
-                title: 'Editing',
-                path: '/views/editing/',
-                collapsable: false,
-                sidebarDepth: 3,
-                children: [
-                ],
-            },
-            {
-                title: 'Search & Replace',
-                path: '/views/search-and-replace/',
-                collapsable: false,
-                sidebarDepth: 3,
-                children: [
-                    '/views/search-and-replace/search-and-replace-single',
-                    '/views/search-and-replace/search-and-replace-multiple'
-                ],
-            },
-            {
-                title: 'Build Systems (Batch Processing)',
-                path: '/views/build-systems/',
-                collapsable: false,
-                sidebarDepth: 3,
-                children: [
-                ],
-            },
-            {
-                title: 'File Management & Navigation',
-                path: '/views/file-management/',
-                collapsable: false,
-                sidebarDepth: 3,
-                children: [
-                    '/views/file-management/file-management-projects',
-                    '/views/file-management/file-management-navigation'
-                ],
-            },
-            {
-                title: 'Customization',
-                path: '/views/customization/',
-                collapsable: false,
-                sidebarDepth: 3,
-                children: [
-                    '/views/customization/settings',
-                    '/views/customization/indentation',
-                    '/views/customization/key_bindings',
-                    '/views/customization/menus',
-                    '/views/customization/color_schemes'
-                ],
-            },
-            {
-                title: 'Extensibility & Automation',
-                path: '/views/',
-                collapsable: false,
-                sidebarDepth: 3,
-                children: [
-                    '/views/extensibility/',
-                    '/views/extensibility/command_palette',
-                    '/views/extensibility/commands',
-                    '/views/extensibility/completions',
-                    '/views/extensibility/macros',
-                    '/views/extensibility/packages',
-                    '/views/extensibility/plugins',
-                    '/views/extensibility/snippets',
-                    '/views/extensibility/syntaxdefs'
-                ],
-            },
-            {
-                title: 'Command Line',
-                path: '/views/command-line/',
-            },
-            {
-                title: 'Reference',
-                path: '/views/reference/',
-                collapsable: true,
-                sidebarDepth: 3,
-                children: [
-                    "/views/reference/projects",
-                    "/views/reference/syntaxdefs",
-                    "/views/reference/color_schemes",
-                    '/views/reference/build_systems',
-                    "/views/reference/key_bindings",
-                    "/views/reference/menus",
-                    "/views/reference/settings",
-                    "/views/reference/completions",
-                    "/views/reference/symbols",
-                    "/views/reference/comments",
-                    "/views/reference/metadata",
-                    "/views/reference/command_palette",
-                    "/views/reference/plugins",
-                    "/views/reference/api",
-                    "/views/reference/commands",
-                    "/views/reference/keyboard_shortcuts_osx",
-                    "/views/reference/keyboard_shortcuts_win"
-                ],
-            },
-        ],
+            // '/': {
+            //     title: 'Introduction',
+            //     path: '/',
+            //     collapsable: false,
+            //     sidebarDepth: 3,
+            //     children: [
+            //         '',
+            //     ],
+            // },
+            '/guide/': [
+                {
+                    title: 'Introduction',
+                    path: '',
+                },
+                {
+                    title: 'Installation',
+                    path: 'installation',
+                },
+                {
+                    title: 'Basic Concepts',
+                    path: 'basic-concepts/',
+                },
+                {
+                    title: 'Editing',
+                    path: 'editing/',
+                    sidebarDepth: 3,
+                    children: [
+                    ],
+                },
+                {
+                    title: 'Search & Replace',
+                    // The theme has a problem if I use a relative path to `/guide/` here.
+                    path: '/guide/search-and-replace/',
+                    collapsable: false,
+                    sidebarDepth: 3,
+                    children: [
+                        'search-and-replace/search-and-replace-single',
+                        'search-and-replace/search-and-replace-multiple'
+                    ],
+                },
+                {
+                    title: 'Build Systems (Batch Processing)',
+                    path: 'build-systems/',
+                    sidebarDepth: 3,
+                    children: [
+                    ],
+                },
+                {
+                    title: 'File Management & Navigation',
+                    // path: '/guide/file-management',
+                    collapsable: false,
+                    sidebarDepth: 3,
+                    children: [
+                        'file-management/file-management-projects',
+                        'file-management/file-management-navigation'
+                    ],
+                },
+                {
+                    title: 'Customization',
+                    path: '/guide/customization',
+                    collapsable: false,
+                    sidebarDepth: 3,
+                    children: [
+                        'customization/settings',
+                        'customization/indentation',
+                        'customization/key_bindings',
+                        'customization/menus',
+                        'customization/color_schemes'
+                    ],
+                },
+                {
+                    title: 'Extensibility & Automation',
+                    path: '/guide/extensibility',
+                    collapsable: false,
+                    sidebarDepth: 3,
+                    children: [
+                        'extensibility/command_palette',
+                        'extensibility/commands',
+                        'extensibility/completions',
+                        'extensibility/macros',
+                        'extensibility/packages',
+                        'extensibility/plugins',
+                        'extensibility/snippets',
+                        'extensibility/syntaxdefs'
+                    ],
+                },
+                // {
+                //     title: 'Command Line',
+                //     path: '/command-line/',
+                // },
+            ],
+            '/reference/': [
+                {
+                    title: 'Reference',
+                    path: '/reference/',
+                    collapsable: false,
+                    sidebarDepth: 1,
+                    // TODO maybe flatten
+                    children: [
+                        'projects',
+                        'syntaxdefs',
+                        'color_schemes',
+                        'build_systems',
+                        'key_bindings',
+                        'menus',
+                        'settings',
+                        'completions',
+                        'symbols',
+                        'comments',
+                        'metadata',
+                        'command_palette',
+                        'plugins',
+                        'api',
+                        'commands',
+                        'keyboard_shortcuts_osx',
+                        'keyboard_shortcuts_win'
+                    ],
+                },
+            ],
+        },
     },
     markdown: {
         lineNumbers: true,
